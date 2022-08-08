7096b82c5d4600cce9238f60a09f5751874ffff2
manoj <mk711325@gmail.com>
2022-08-09 00:58:23 +0545

    Window name changed

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(SCREEN_W, SCREEN_H, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_W, SCREEN_H, "Computer Graphics III/I", NULL, NULL);
     if (window == NULL)
     {
         printf("Failed to create a window");
